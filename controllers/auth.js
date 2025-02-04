10aa040f76064edd9fb802a5e99f84a98e822740
Magno <jmagno.dutra@hotmail.com>
2025-02-04 08:34:39 -0300

    Create user - basic setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
 async function register(req, res) {
-  res.send("register user");
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json(user);
 }
 
 async function login(req, res) {
