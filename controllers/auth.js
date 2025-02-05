8ced15a44553925d591d0dda2fb61d19d4927f1b
Magno <jmagno.dutra@hotmail.com>
2025-02-05 07:56:32 -0300

    Login controller setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors/index.js");
 
 async function register(req, res) {
   const user = await User.create({ ...req.body });
@@ -10,7 +11,21 @@ async function register(req, res) {
 }
 
 async function login(req, res) {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 }
 
 module.exports = {
