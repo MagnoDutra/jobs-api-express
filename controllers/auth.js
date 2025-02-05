1ed39a9f3cc9aaedfed734f89083ef5a1bd52538
Magno <jmagno.dutra@hotmail.com>
2025-02-05 07:22:15 -0300

    Mongoose Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const bcrypt = require("bcryptjs");
 
 async function register(req, res) {
-  const { name, email, password } = req.body;
+  const user = await User.create({ ...req.body });
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json(user);
 }
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,14 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide a password"],
     minlength: 6,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  // vc n precisa do next se for async/await
 });
 
 module.exports = mongoose.model("User", UserSchema);
